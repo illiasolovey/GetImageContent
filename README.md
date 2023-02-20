b3977602e74b8296465b47e1e90b52ccb39fd991
Illia Solovey <114160085+illiasolovey@users.noreply.github.com>
2023-02-20 20:54:49 +0100

    Function extension
    GetImageContent has been enhanced and replaced with ObjectAnalysis function, which includes labels detection and creating bounding boxes around found objects.

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
-# Get Image Content Lambda Function
-Serverless Lambda solution to get content of any image via API request using AWS Rekognition service.
+# Serverless Object Analysis
+Serverless solution to analyse the content of an image via API request using AWS Rekognition service.
+
+### Description
+Object Analyser is an AWS Lambda function that utilizes AWS Rekognition service to analyze the contents within the given image. It is capable of automated image labeling and drawing bounding boxes around found objects.
 
 The following function is a part of [serverless-object-recognition](https://github.com/illiasolovey/serverless-object-recognition) project.
 
-## Getting started from the command line
+### Requirements
+
+- [.NET 6 SDK](https://dotnet.microsoft.com/download/dotnet/6.0)
+- [AWS account](https://aws.amazon.com/)
+- AWS CLI Tools installed on your machine.
+
+### Getting started from the command line
+
+1. Clone this repository:
+    ```
+    git clone https://github.com/illiasolovey/ObjectAnalysis.git
+    ```
 
 1. Install Amazon.Lambda.Tools Global Tools if not already installed.
 
@@ -14,34 +28,39 @@ The following function is a part of [serverless-object-recognition](https://gith
 1. Configure AWS profile, see: [AWS CLI Configuration](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html)
 
 1. Add configuration files: `appsettings.json` and `aws-lambda-tools-default.json`
+   
    - Inside aws-lambda-tools-default.json add the followings lines and configure your own lambda:
    
-    ```
-    {
-        "profile": "{your configured AWS profile}",
-        "region": "{service region}",
-        "configuration": "Release",
-        "function-architecture": "x86_64",
-        "function-runtime": "dotnet6",
-        "function-role": "{IAM role for your function}",
-        "function-memory-size": 256,
-        "function-timeout": 30,
-        "function-handler": "{GetImageContent::GetImageContent.Function::FunctionHandler}"
-    }
-    ```
-    Function logic code is stored inside "GetImageContent::GetImageContent.Function::FunctionHandler", you can change its location by changing `"function-handler"` property
+        ```
+        {
+            "profile": "{your configured AWS profile}",
+            "region": "{service region}",
+            "configuration": "Release",
+            "function-architecture": "x86_64",
+            "function-runtime": "dotnet6",
+            "function-role": "{IAM role for your function}",
+            "function-memory-size": 256,
+            "function-timeout": 30,
+            "function-handler": "ObjectAnalysis::ObjectAnalysis.Function::FunctionHandler"
+        }
+        ```
+        Function logic code is stored inside "ObjectAnalysis::ObjectAnalysis.Function::FunctionHandler", you can change its location by changing `"function-handler"` property
     - Configure S3 bucket in appsettings.json
 
-    ```
-    {
-        "LambdaConfiguration": {
-            "BucketName": "{S3 bucket name}"
+        ```
+        {
+            "StorageConfiguration": {
+                "GetBucket": {Bucket to get desired object},
+                "PutBucket": {Bucket to put updated object}
+            }
         }
-    }
-    ```
+        ```
 
 1.  Deploy function to AWS Lambda
 
     ```
-        dotnet lambda deploy-function
+        dotnet lambda deploy-function ObjectAnalysis
     ```
+    This command will generate new Lambda function on your account, provided that specified name is not already in use.
+
+1.  After successful deployment, access your AWS Lambda console and review your newly created function.
--- src/Function.cs
+++ src/Function.cs
@@ -1,43 +1,155 @@
 using Amazon.Lambda.Core;
 using Amazon.Rekognition;
 using Amazon.Rekognition.Model;
+using Amazon.S3;
+using Amazon.S3.Model;
 using Microsoft.Extensions.Configuration;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Processing;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
-namespace GetImageContent;
-
-public class Function
+namespace ObjectAnalysis
 {
-    public IConfiguration FunctionConfiguration { get; private set; } = null!;
-
-    private void ConfigureSettings()
+    class Function
     {
-        FunctionConfiguration = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json", optional: false)
-            .Build();
-    }
+        public IConfiguration FunctionConfiguration { get; private set; } = null!;
 
-    public Function() =>
-        ConfigureSettings();
+        private void ConfigureSettings()
+        {
+            FunctionConfiguration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+        }
 
-    public async Task<DetectLabelsResponse> FunctionHandler(string filename, ILambdaContext context)
-    {
-        string bucketName = FunctionConfiguration["LambdaConfiguration:BucketName"];
-        var rekognitionClient = new AmazonRekognitionClient();
-        var response = await rekognitionClient.DetectLabelsAsync(
-            request: new DetectLabelsRequest
+        public Function() =>
+            ConfigureSettings();
+
+        public async Task<string> FunctionHandler(string objectKey, ILambdaContext context)
+        {
+            string getBucket = FunctionConfiguration["StorageConfiguration:UploadBucketName"];
+            using var s3Client = new AmazonS3Client();
+
+            using var objectResponse = await s3Client.GetObjectAsync(new GetObjectRequest
             {
-                Image = new Image
+                BucketName = getBucket,
+                Key = objectKey
+            });
+            string objectType = objectResponse.Headers.ContentType;
+
+            using var objectStream = await GetObjectMemoryStream(objectResponse);
+            DetectLabelsResponse detectedLabels = await DetectLabels(objectStream);
+
+            byte[] objectStreamBytes = objectStream.ToArray();
+            using var memoryStream = new MemoryStream(objectStreamBytes);
+            using var image = SixLabors.ImageSharp.Image.Load(memoryStream);
+            DrawBoundingBoxes(image, detectedLabels);
+            using var outputObjectStream = new MemoryStream();
+            image.Save(outputObjectStream, GetObjectImageFormat(objectType));
+
+            string putBucket = FunctionConfiguration["StorageConfiguration:DownloadBucketName"];
+            await s3Client.PutObjectAsync(new PutObjectRequest
+            {
+                BucketName = putBucket,
+                Key = objectKey,
+                InputStream = outputObjectStream
+            });
+            return GetPresignedUrl(
+                s3Client: s3Client,
+                bucketName: putBucket,
+                objectKey: objectKey,
+                expiresInMin: 20
+            );
+        }
+
+        private async Task<MemoryStream> GetObjectMemoryStream(GetObjectResponse response)
+        {
+            using var objectMemoryStream = new MemoryStream();
+            using (Stream responseStream = response.ResponseStream)
+                await responseStream.CopyToAsync(objectMemoryStream);
+            return objectMemoryStream;
+        }
+
+        private async Task<DetectLabelsResponse> DetectLabels(MemoryStream objectStream)
+        {
+            string bucketName = FunctionConfiguration["StorageConfiguration:UploadBucketName"];
+            var rekognitionClient = new AmazonRekognitionClient();
+            var response = await rekognitionClient.DetectLabelsAsync(
+                request: new DetectLabelsRequest
                 {
-                    S3Object = new S3Object
+                    Image = new Amazon.Rekognition.Model.Image
                     {
-                        Bucket = bucketName,
-                        Name = filename
-                    }
+                        Bytes = objectStream
+                    },
+                    MinConfidence = 70
+                }
+            );
+            return response;
+        }
+
+        private void DrawBoundingBoxes(SixLabors.ImageSharp.Image image, DetectLabelsResponse detectResponse)
+        {
+            foreach (var label in detectResponse.Labels)
+            {
+                foreach (var instance in label.Instances)
+                {
+                    var bound = instance.BoundingBox;
+                    int x = (int)(image.Width * bound.Left);
+                    int y = (int)(image.Height * bound.Top);
+                    int width = (int)(image.Width * bound.Width);
+                    int height = (int)(image.Height * bound.Height);
+                    var rectangle = new Rectangle(x, y, width, height);
+                    var points = new PointF(x, y);
+                    DrawBoundingRectangle(image, label, rectangle, points);
                 }
             }
-        );
-        context.Logger.LogInformation($"API request at {DateTime.Now}. Requested file trace: \"{bucketName}\\{filename}\"");
-        return response;
+        }
+
+        private void DrawBoundingRectangle(SixLabors.ImageSharp.Image image, Label label, Rectangle rectangle, PointF points)
+        {
+            var labelName = label.Name;
+            var confidence = Math.Round(label.Confidence, 2);
+            var text = $"{labelName} ({confidence * 100}%)";
+            var pen = new Pen(Color.Red, 5);
+            var font = SystemFonts.CreateFont("DejaVu Sans", 20, FontStyle.Bold);
+            var color = new SixLabors.ImageSharp.PixelFormats.Rgba32(255, 255, 255);
+            var brush = Brushes.Solid(color);
+            image.Mutate(ctx => ctx
+                .Draw(pen, rectangle)
+                .DrawText(label.Name, font, brush, points)
+            );
+        }
+
+        private IImageFormat GetObjectImageFormat(string contentType)
+        {
+            switch (contentType)
+            {
+                case "image/jpg":
+                case "image/jpeg":
+                    return SixLabors.ImageSharp.Formats.Jpeg.JpegFormat.Instance;
+                case "image/png":
+                    return SixLabors.ImageSharp.Formats.Png.PngFormat.Instance;
+                case "image/gif":
+                    return SixLabors.ImageSharp.Formats.Gif.GifFormat.Instance;
+                case "image/bmp":
+                    return SixLabors.ImageSharp.Formats.Bmp.BmpFormat.Instance;
+                default:
+                    throw new ArgumentException($"{contentType} is not supported.");
+            }
+        }
+
+        private string GetPresignedUrl(AmazonS3Client s3Client, string bucketName, string objectKey, int expiresInMin)
+        {
+            DateTime expiration = DateTime.UtcNow.AddMinutes(expiresInMin);
+            var request = new GetPreSignedUrlRequest
+            {
+                BucketName = bucketName,
+                Key = objectKey,
+                Expires = expiration
+            };
+            return s3Client.GetPreSignedURL(request);
+        }
     }
 }
